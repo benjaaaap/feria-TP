349170ca470ac51e53ebbbc51b948c4187345828
benjaaaap <162449510+benjaaaap@users.noreply.github.com>
2024-10-02 10:34:42 -0300

    agregando

---
+++ Feria/js.js
@@ -0,0 +1,11 @@
+function musica(){
+    alert ("NOSE MUSICA!")
+}
+
+function mostrar(){
+    document.getElementById('list').style.display = 'block';
+}
+
+function ocultar(){
+    document.getElementById('exe').style.display = 'none'
+}
